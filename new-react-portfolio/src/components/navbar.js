efe4968e86bfd81582e63cbe8ae431b704cb4394
unknown <temple0715@gmail.com>
2022-10-12 01:06:51 -0500

    completed navbar css

--- new-react-portfolio/src/components/navbar.js
+++ new-react-portfolio/src/components/navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/ReorderIcon";
 
 function navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
     <div className='navbar'>
         <div className='toggleButtton'>
-            <button></button>
+            <button onClick={() => {setExpandNavbar((prev) => !prev)}}> <ReorderIcon />
+            </button>
         </div>
         <div className='links'>
             <Link to="/"> Home </Link>
